4b3ce77a496b5878348575073d9414f83cb2b4df
Asif3359 <asifahammed359@gmail.com>
2023-12-10 03:53:03 +0600

    Added mongodb

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+// require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ylhegt.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const ProjectCollection = client.db('AsifAhammed').collection('Projects');
+
+
+        app.get('/projects', async (req, res) => {
+            const cursor = ProjectCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send("Asif Ahammed Server Is Running");
+});
+
+
+app.listen(port, () => {
+    console.log(`Asif Ahammed Server Is Running :${port}`);
+});
